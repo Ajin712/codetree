18227b9c8874a2bb47b2e4871bea55734ddb1510
Ajin712 <heoajin1@gmail.com>
2026-07-23 16:50:31 +0900

    [Passed] 범위 안의 수 (130ms, 10MB)

--- trail1/범위 안의 수/number-in-range.java
+++ trail1/범위 안의 수/number-in-range.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
-        if (a <= 10 && a <= 20) {
+        if (a >= 10 && a <= 20) {
             System.out.println("yes");
         }
         else {
