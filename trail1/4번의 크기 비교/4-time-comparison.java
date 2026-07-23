aa10fbe8004829f0a55a3fe20327bd0c0a4b4321
Ajin712 <heoajin1@gmail.com>
2026-07-23 16:37:45 +0900

    [Passed] 4번의 크기 비교 (146ms, 10MB)

--- trail1/4번의 크기 비교/4-time-comparison.java
+++ trail1/4번의 크기 비교/4-time-comparison.java
@@ -5,28 +5,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int b = sc.nextInt();
-        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
 
-        if (a >= b) {
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
         if (a > b) {
             System.out.println(1);
         }
         else {
             System.out.println(0);
         }
-        if (a <= b) {
+        if (a > c) {
             System.out.println(1);
         }
         else {
             System.out.println(0);
         }
-        if (a < b) {
+        if (a > d) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+        if (a > e) {
             System.out.println(1);
         }
         else {
