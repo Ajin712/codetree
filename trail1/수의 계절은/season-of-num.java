b18b1b09d3abbf7d7fcc485b4ea212f98f7b59e9
Ajin712 <heoajin1@gmail.com>
2026-07-23 17:28:28 +0900

    [Wrong Answer] 수의 계절은 (142ms, 10MB)

---
+++ trail1/수의 계절은/season-of-num.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+
+        if (m > 2 && m <= 5) {
+            System.out.print("Spring");
+        }
+        else if (m <= 8) {
+            System.out.print("Summer");
+        }
+        else if (m <= 11) {
+            System.out.print("Fall");
+        }
+    }
+}
