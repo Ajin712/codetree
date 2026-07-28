89a30b792755d067e48e9c547a35a6e7d3ab827b
Ajin712 <heoajin1@gmail.com>
2026-07-28 22:11:49 +0900

    [Passed] 범위 안의 수 2 (138ms, 10MB)

---
+++ trail1/범위 안의 수 2/number-in-range-2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int cnt = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            int a = sc.nextInt();
+
+            if (a >= 0 && a <= 200) {
+                sum += a;
+                cnt++;
+            }
+        }
+        System.out.printf(sum + " %.1f", (double)sum / cnt);
+    } 
+}
