34ea5a30fe0da495862600ced9567ec860a855bc
Ajin712 <heoajin1@gmail.com>
2026-09-03 03:05:57 +0900

    [Passed] 몫 출력하기 (124ms, 10MB)

---
+++ trail1/몫 출력하기/print-share.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+
+        while (true) {
+            int N = sc.nextInt();
+
+            if (N % 2 == 0) {
+                System.out.println(N / 2);
+                cnt++;
+                if (cnt == 3) {
+                    break;
+                }
+            }
+        }
+    }
+}
